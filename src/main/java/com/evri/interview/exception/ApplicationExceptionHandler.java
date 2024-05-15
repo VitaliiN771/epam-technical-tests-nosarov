c8823effbd5ff008978735611fe2add6f20b8c9d
vitalii_nosarov <vitalii.nosarov@ge.com>
2024-05-15 17:16:36 +0300

    Refactor

--- src/main/java/com/evri/interview/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/evri/interview/exception/ApplicationExceptionHandler.java
@@ -3,6 +3,7 @@ package com.evri.interview.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,10 +12,10 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class ApplicationExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
         return exception.getBindingResult().getAllErrors().stream().collect(
                 Collectors.toMap(
@@ -24,7 +25,7 @@ public class ExceptionHandler {
         );
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFoundException(NotFoundException exception) {
         return exception.getMessage();
